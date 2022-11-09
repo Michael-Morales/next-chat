38f7df0d4da73629fed62610e5eacb948163573a
mmorales <mt.morales@hotmail.fr>
2022-11-09 10:32:43 +0100

    added presence to chat component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,8 @@
 import { KeyboardEventHandler, useState, useRef, useEffect } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { configureAbly, useChannel } from "@ably-labs/react-hooks";
+import { configureAbly, useChannel, usePresence } from "@ably-labs/react-hooks";
+import { useSession } from "next-auth/react";
 
 import { chatMessageSchema, IChatMessage } from "@lib/validation/chat";
 
@@ -17,9 +18,20 @@ interface IMessage {
 }
 
 export default function Chat() {
+  const { data: session } = useSession();
   const [messages, setMessages] = useState<IMessage[]>([]);
+  const [status, setStatus] = useState<string | null>(null);
   const [channel] = useChannel("main-room", ({ clientId, data, id }) => {
     setMessages((prev) => [...prev, { username: clientId, data, id }]);
+  });
+  usePresence("main-room", "", ({ clientId, action }) => {
+    if (session?.user.username !== clientId) {
+      if (action === "enter") {
+        setStatus(`${clientId} has entered the chat`);
+      } else if (action === "leave") {
+        setStatus(`${clientId} has left the chat`);
+      }
+    }
   });
   const emptyRef = useRef<HTMLDivElement>(null);
   const {
@@ -51,8 +63,25 @@ export default function Chat() {
     emptyRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
+  useEffect(() => {
+    if (status) {
+      const timeoutId = setTimeout(() => {
+        setStatus(null);
+      }, 2000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [status]);
+
   return (
-    <div className="flex h-[calc(100vh-72px)] flex-col justify-between">
+    <div className="relative flex h-[calc(100vh-72px)] flex-col justify-between">
+      {status && (
+        <div className="absolute top-2 left-1/2 -translate-x-1/2 rounded-full bg-sky-500 px-4 py-1 text-sm font-semibold text-white">
+          {status}
+        </div>
+      )}
       <div className="flex h-full flex-col gap-y-2 overflow-y-auto bg-zinc-50 p-4">
         {messages.map(({ id, username, data }) => (
           <Message key={id} username={username} message={data} />
